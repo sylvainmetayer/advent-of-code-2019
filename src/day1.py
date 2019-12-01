1e487a8ad97e732fa9eefa89d75dcd8745338cfa
Sylvain METAYER <contact@sylvainmetayer.fr>
2019-12-01 14:49:39 +0100

    feat(day1): second part

--- src/day1.py
+++ src/day1.py
@@ -12,25 +12,37 @@ class Solver:
 
     def __init__(self):
         self.sum = 0
-        self.file = None
-
-    def open(self, file):
-        self.file = open(file, "r")
 
     def solve(self, number) -> int:
         if type(number) != int:
             raise SolverError("'" + number + "' is not a number")
-        res = floor(number / 3) - 2
-        self.sum += res
-        return res
+        return floor(number / 3) - 2
 
-    def process(self):
-        for line in self.file:
-            self.solve(int(line))
+    def solve_recursive(self, number):
+        if type(number) != int:
+            raise SolverError("'" + number + "' is not a number")
+        res = floor(number / 3) - 2
+        if res <= 0:
+            return 0
+        return res + self.solve_recursive(res)
+
+    def process(self, filename: str):
+        self.sum = 0
+        with open(filename, "r") as file:
+            for line in file:
+                self.sum += self.solve(int(line))
+        return self.sum
+
+    def process_recursive(self, filename: str):
+        self.sum = 0
+        with open(filename, "r") as file:
+            for line in file:
+                self.sum += self.solve_recursive(int(line))
         return self.sum
 
 
 if __name__ == '__main__':
     s = Solver()
-    s.open(os.path.dirname(os.path.realpath(__file__)) + "/../inputs/day1.txt")
-    print(s.process())
+    f = os.path.dirname(os.path.realpath(__file__)) + "/../inputs/day1.txt"
+    print(f'Solve simple : {s.process(f)}')
+    print(f'Solve recursion : {s.process_recursive(f)}')
--- tests/day1.py
+++ tests/day1.py
@@ -16,15 +16,6 @@ class SolverTest(TestCase):
 
     def test_day1(self):
         self.assertEqual(0, self.s.sum)
-        self.assertIsNone(self.s.file)
-
-    def test_open_file(self):
-        self.s.open(os.path.dirname(os.path.realpath(__file__)) + "/../inputs/day1.txt")
-        self.assertIsNotNone(self.s.file)
-
-    def test_open_file_fail(self):
-        with self.assertRaises(FileNotFoundError):
-            self.s.open("inputs/day1.txt")
 
     def test_simple_solver(self):
         self.assertEqual(2, self.s.solve(12))
@@ -36,13 +27,31 @@ class SolverTest(TestCase):
         self.assertEqual(33583, self.s.solve(100756))
 
     def test_solver_sum(self):
-        self.s.solve(12)
-        self.s.solve(12)
+        self.s.sum += self.s.solve(12)
+        self.s.sum += self.s.solve(12)
         self.assertEqual(4, self.s.sum)
 
     def test_solve_bad_value(self):
         with self.assertRaises(SolverError):
             self.s.solve("test")
+
+    def test_solver_recursive(self):
+        self.assertEqual(2, self.s.solve_recursive(14))
+
+    def test_solver_recursive_edge_case(self):
+        self.assertEqual(966, self.s.solve_recursive(1969))
+
+    def test_solver_recursive_sum(self):
+        self.s.sum += self.s.solve_recursive(1969)
+        self.assertEqual(966, self.s.sum)
+
+    def test_process_solve(self):
+        f = os.path.dirname(os.path.realpath(__file__)) + "/../inputs/day1.txt"
+        self.assertEqual(3363033, self.s.process(f))
+
+    def test_process_solve_recursive(self):
+        f = os.path.dirname(os.path.realpath(__file__)) + "/../inputs/day1.txt"
+        self.assertEqual(3363033, self.s.process(f))
 
 
 if __name__ == '__main__':
