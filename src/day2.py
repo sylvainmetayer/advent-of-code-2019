329eb2eaaece1e4e100d8af078d5a5fdd393f3a8
Sylvain METAYER <contact@sylvainmetayer.fr>
2019-12-02 20:51:01 +0100

    parametrized tests

--- src/day2.py
+++ src/day2.py
@@ -14,55 +14,71 @@ class Solver:
 
     def get_sublist(self, number: int = 4, offset: int = 0) -> list:
         sublist = self.items[offset: offset + number]
-        print(sublist, sublist[0])
-        return [sublist[0]] if sublist[0] == 99 and len(sublist) >= 1 else sublist
+        return [sublist[0]] if len(sublist) >= 1 and sublist[0] == 99 else sublist
 
     def handle_sublist(self, sublist: list):
-        if type(sublist) != list or len(sublist) != 4:
-            raise SolverError("Need 4 item in a list to process")
-        operation, index1, index2, res_index = sublist
-        if operation == 99:
+        if type(sublist) != list:
+            raise SolverError("Need a list to process")
+        if len(sublist) == 1 and sublist[0] == 99:
             return None
+        if len(sublist) != 4:
+            raise SolverError("Need 4 items to process")
+        operation, index1, index2, res_index = sublist
         res = self.items[index1] + self.items[index2] if operation == 1 else self.items[index1] * self.items[index2]
         self.items[res_index] = res
         return res
 
     def handle_find_item(self, sublist: list, seek: int):
-        print(sublist)
-        if type(sublist) != list or len(sublist) != 4:
-            raise SolverError("Need 4 item in a list to process")
+        if type(sublist) != list:
+            raise SolverError("Need a list to process")
+        if len(sublist) == 1 and sublist[0] == 99:
+            return None
+        if len(sublist) != 4:
+            print(sublist)
+            raise SolverError("test")
         operation, noun, verb, res_index = sublist
 
-        if operation == 99:
-            return None
-        return self.items[noun] + self.items[verb] if operation == 1 else self.items[noun] * self.items[verb]
+        if not 0 <= noun <= 99 or not 0 <= verb <= 99:
+            self.items[res_index] = self.items[noun] + self.items[verb] \
+                if operation == 1 else self.items[noun] * self.items[verb]
 
-    def find_item(self, filename: str, seek: int = 19690720):
-        with open(filename, "r") as file:
-            line = file.read()
-            self.items = list(map(lambda x: int(x), line.split(',')))
-            i = 0
+        print(f'operation {operation} : noun({noun}): {self.items[noun]} - '
+              f'verb({verb}): {self.items[verb]} - res({res_index}):{self.items[res_index]}')
+
+        return self.items[res_index]
+
+    def find_item(self, seek: int):
+        backup = self.items.copy()
+        i = 0
+        sublist = self.get_sublist(4, i)
+        res = self.handle_find_item(sublist, seek)
+        while res != seek:
             sublist = self.get_sublist(4, i)
+            i += len(sublist) if len(sublist) > 0 else 1
             res = self.handle_find_item(sublist, seek)
-            while res != seek:
-                sublist = self.get_sublist(4, i)
-                i += len(sublist)
-                res = self.handle_find_item(sublist, seek)
-                print(res)
-        return sublist[1], sublist[2]
+            if res is None:
+                self.items = backup
+            print(f'i = {i} : {sublist} => "{res}"')
+        if len(sublist) == 2:
+            return sublist[1], sublist[2]
+        return None
 
-    def process(self, filename: str):
+    def process(self):
+        i = 0
+        while self.handle_sublist(self.get_sublist(4, i)) is not None:
+            i += 4
+        return self.items[0]
+
+    def init(self, filename: str):
         with open(filename, "r") as file:
             line = file.read()
-            self.items = list(map(lambda x: int(x), line.split(',')))
-            i = 0
-            while self.handle_sublist(self.get_sublist(4, i)) is not None:
-                i += 4
-        return self.items[0]
+            self.items = list(map(lambda x: int(x), line.split(",")))
 
 
 if __name__ == '__main__':
     s = Solver()
     f = os.path.dirname(os.path.realpath(__file__)) + "/../inputs/day2.txt"
-    print(f'Solve simple : {s.process(f)}')
-    print(f'Solve find item : {s.find_item(f, 19690720)}')
+    s.init(f)
+    print(f'Solve simple : {s.process()}')
+    s.init(f)
+    print(f'Solve find item : {s.find_item(19690720)}')
--- tests/day2.py
+++ tests/day2.py
@@ -46,7 +46,20 @@ class SolverTest(TestCase):
 
     def test_handle_sublist_halt(self):
         self.s.items = [1, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]
-        self.assertIsNone(self.s.handle_sublist([99, 30, 40, 50]))
+        self.assertIsNone(self.s.handle_sublist([99]))
+
+    def test_part1_example(self):
+        params = [
+            ([1, 1, 1, 4, 99, 5, 6, 0, 99], [30, 1, 1, 4, 2, 5, 6, 0, 99]),
+            ([2, 4, 4, 5, 99, 0], [2, 4, 4, 5, 99, 9801]),
+            ([2, 3, 0, 3, 99], [2, 3, 0, 6, 99]),
+            ([1, 0, 0, 0, 99], [2, 0, 0, 0, 99])
+        ]
+        for init, expect in params:
+            with self.subTest(init=init, expect=expect):
+                self.s.items = init
+                self.s.process()
+                self.assertEqual(expect, self.s.items)
 
     def test_handle_sublist_without_list(self):
         with self.assertRaises(SolverError):
